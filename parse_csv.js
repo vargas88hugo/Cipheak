74fe101aa15ce143587a203a8102ed9853a85117
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-02 23:33:39 -0500

    [Fix] Refactored code

--- parse_csv.js
+++ parse_csv.js
@@ -20,20 +20,28 @@ obj.from.path('/home/hugodev/Programming/Cypheak/words.csv').to.array((data) => 
 /**
  * Method that parse each word of the CSV list
  */
-let parseRoot = (data, i, j) => {
-  let vocals = ['a', 'e', 'i', 'o', 'u'];
+const parseRoot = (data, i, j) => {
+  const vocals = ['a', 'e', 'i', 'o', 'u'];
+  const parsers = {
+    "e": /[əæ]/g,
+    "i": /[ı]/g,
+    "o": /[ðø]/g,
+    "u": /[ŭ]/g,
+    "n": /[ħñ]/g,
+    "": /[ ,'ʻ-]/g,
+    "v": /[w]/g,
+    "k": /[q]/g
+  }
+  const entries = Object.entries(parsers);
+  
   
   data[i][j] = data[i][j].toLowerCase();
+
   data[i][j] = data[i][j].normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-  data[i][j] = data[i][j].replace(/[əæ]/g, "e");
-  data[i][j] = data[i][j].replace(/[ı]/g, "i");
-  data[i][j] = data[i][j].replace(/[ðø]/g, "o");
-  data[i][j] = data[i][j].replace(/[ŭ]/g, "u");
-  data[i][j] = data[i][j].replace(/[ħñ]/g, "n");
-  data[i][j] = data[i][j].replace(/[ ,'ʻ-]/g, "");
-  data[i][j] = data[i][j].replace(/[w]/g,"v");
-  data[i][j] = data[i][j].replace(/[q]/g, "k");
-  
+  for (const [charac, regex] of entries) {
+    data[i][j] = data[i][j].replace(regex, charac);
+  }
+
   while (vocals.includes(data[i][j][data[i][j].length - 1])) {
     data[i][j] = data[i][j].substring(0, data[i][j].length - 1);
   }
@@ -45,20 +53,17 @@ let parseRoot = (data, i, j) => {
 let loopData = (data) => {
   let count = 1;
 
-  for (let i = 1; i <= 82; i++) {
+  for (let i = 1; i <= 60; i++) {
     const myData2 = [];
     myData1.push(count++);
 
     myData2.push(data[i][0]);
     for (let j = 0; j <= 82; j++) {
       if (data[i][j] !== undefined && data[i][j] !== "" && data[i][j].length > 2 && data[i][j].length <= 10) {
-
 	parseRoot(data, i, j);
-	
 	if (data[i][j].length < 2) {
 	  continue;
 	}
-	
 	myData2.push(data[i][j]);
       }
     }
