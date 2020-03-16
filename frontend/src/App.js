e5ef5450be7b99e5739ad68f23af8daeba7d7f26
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-15 20:17:53 -0500

    [Feature] Added pdf generator from Juan repository

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PdfGenerator from './component/pdf';
+
+function App() {
+  return (
+    <div className="App">
+      <PdfGenerator />
+    </div>
+  );
+}
+
+export default App;
