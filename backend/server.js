6eb419358ca55db9e25e84f9cacea54d96f55c1c
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-19 18:22:36 -0500

    [Feature] Refactored code and added parsing and cors middlewares

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const properties = require('./config/properties');
 const authRoutes = require('./google/auth.routes');
 const keys = require('./config/keys');
-require('./google/auth.model');
-require('./google/auth.controller');
+require('./authGoogle/auth.model');
+require('./authGoogle/auth.controller');
 
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
+
 app.use(cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 );
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
