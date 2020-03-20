505742f59386d18b57cd2863b2c734ab285e7060
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-20 09:01:38 -0500

    [Feature] Implemented popup google authentication on jade files

--- backend/authGoogle/auth.routes.js
+++ backend/authGoogle/auth.routes.js
@@ -8,7 +8,18 @@ module.exports = (app) => {
   );
   
   app.get('/auth/google/callback', passport.authenticate('google', {
-    successRedirect: '/', failureRedirect: '/'}));
+    failureRedirect: '/auth/google/callback'}), (req, res) => {
+      res.redirect('/after-auth.html')
+    });
+
+  app.get('/popup', (req, res, next) => {
+    res.render('auth-popup-callback', {layout: false});
+  });
+
+  app.get('/popup-done', (req, res, next) => {
+    var url = (req.user && req.session.firstLogin) ? '/' : '/';
+    res.redirect(url);
+  });
 
   app.get('/api/logout', (req, res) => {
     req.logout();
